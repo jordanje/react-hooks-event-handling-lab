c20cc88e7bd37280e275c2e0384247eff510fba2
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-30 11:17:50 -0400

    commit tree 2f53106e08e0bdc7575d4836c732f15be84e1b0d

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function passwordChange(event){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={passwordChange}/>
+        </div>
     )
 }
 
