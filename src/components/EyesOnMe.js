00aeb069c331ef02d5c0732f1aa12d48d6c9d46d
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-30 11:18:00 -0400

    tests passing

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function focusButton(event){
+        console.log("Good!")
+    }
+
+    function blurButton(event){
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onFocus={focusButton} onBlur={blurButton}>Eyes on me</button>
+        </div>
+    )
+}
+
+
+export default EyesOnMe
